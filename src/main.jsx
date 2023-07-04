e818b3d42de755a74f3991b53969566667da4c9a
Md. Bariul Munshi <bariulsoft@gmail.com>
2023-07-04 22:07:39 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './component/Home/Home.jsx';
+import Main from './component/Layout/Main.jsx';
+import OrderReview from './component/OrderReview/OrderReview.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader:()=>fetch('tshirts.json')
+      },
+      {
+        path:'/review',
+        element:<OrderReview></OrderReview>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
